c601b2d0da1cae241d1807d991571fd12665c65c
asir khan <ashir16592@gmail.com>
2024-09-08 16:34:39 +0530

    add calculator code

--- node.js
+++ node.js
@@ -1,19 +1,28 @@
-// function f() {
-//   document.getElementById("C").style.color = "red";
-//   document.getElementById("C").style.backgroundColor = "blue";
-// }
-function f() {
-  let string = "";
-  let buttons = document.querySelectorAll(".k");
-  Array.from(buttons).forEach((k) => {
-    k.addEventListener("click", (e) => {
-      if (e.target.innerHTML == "=") {
-        string = eval(string);
-        document.querySelector("disp").value = string;
-      }
-      console.log(e.target);
-      string = string + e.target.innerHTML;
-      document.querySelector("disp").value = string;
-    });
-  });
+function appendToDisplay(input) {
+  let display = document.getElementById("disp");
+  display.value += input;
+}
+console.log(input);
+function clearDisplay(input) {
+  let display = document.getElementById("disp");
+  display.value = "";
+}
+
+function calculate(input) {
+  let display = document.getElementById("disp");
+  display.value = eval(display.value);
+}
+
+function percent(input) {
+  let display = document.getElementById("disp");
+  display1 = parseFloat(display.value) / 100;
+  display.value = display1;
+}
+
+function c1() {
+  document.getElementById("C");
+  let display = document.getElementById("disp");
+  let v = display.value;
+  let v1 = v.slice(0, v.length - 1);
+  display.value = v1;
 }
